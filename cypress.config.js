b855c3fdb540473eb058fa3255a429430c0321d7
Chamleck <chamlecks@gmail.com>
2022-11-28 11:44:41 +0200

    set up faker

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+      console.log(config) // see everything in here!
+     
+      
+      // modify config values
+
+
+      config.defaultCommandTimeout = 10000
+      config.pageLoadTimeout = 100000
+      config.baseUrl = 'http://localhost:3000'
+      
+
+      
+      // modify env var value
+      config.env.ENVIRONMENT = 'main'
+      
+      // IMPORTANT return the updated config object
+      return config
+    }
+  }
+})
